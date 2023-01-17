6fab652fc4f1fe5527f500027657904d8a07c246
David Saifer <spyrotheultimate@gmail.com>
2023-01-16 17:32:02 -0800

    working bounce

--- days/script-Jan12.js
+++ days/script-Jan12.js
@@ -12,8 +12,9 @@ canvas.height = height;
 
 
 var baseBall = {
-    pos: new Vector(width / 2, 0),
+    pos: new Vector(width / 2, height / 2),
     vel: new Vector(0, 5),
+    accel: new Vector(0, .2),
     Hue: 192,
     Lightness: 45,
     radius:20
@@ -35,6 +36,7 @@ var t1 = Date.now()
 var t2 = Date.now()
 
 const noiseSave = ctx.getImageData(0, 0, width, height)
+const displaySave = ctx.createImageData(noiseSave)
 
 const noiseSaveData = noiseSave.data
         
@@ -42,7 +44,7 @@ i = 0
 for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++) {
 
-        curNoiseVal = noise.simplex3(x / 300, y / 300, Date.now() / 10000)
+        curNoiseVal = noise.simplex3(x / 200, y / 200, Date.now() / 10000)
 
         curVal = ((Math.sqrt(Math.sqrt(curNoiseVal)) + 1)* 1/2) * 255
         
@@ -55,7 +57,30 @@ for (let y = 0; y < height; y++) {
     
 }
 
-            
+function noiseGen(x, y, z) {
+    curNoiseVal = noise.simplex3(x / 200, y / 200, z / 10000)
+
+    curVal = ((Math.sqrt(Math.sqrt(curNoiseVal)) + 1)* 1/2) * 255
+
+    return curVal
+}
+
+for (let index = 0; index < 10; index++) {
+
+    do {
+        possibleX = Math.random() * width
+        possibleY = Math.random() * height
+    } while (noiseSaveData[Dimension2to1(possibleX, possibleY, width) * 4] != 0);
+
+    newball = {...baseBall}
+    
+    newball.pos = new Vector(possibleX, possibleY)
+
+    randAngle = Math.random() * Math.PI * 2
+    newball.vel = new Vector(Math.cos(randAngle), Math.sin(randAngle))
+    ballArray.push(newball)
+    
+}  
 
 
 function MainLoop() {
@@ -65,8 +90,6 @@ function MainLoop() {
         return
     }
 
-    const imageData = ctx.getImageData(0, 0, width, height);
-    const data = imageData.data;
 
     t1 = Date.now()
     var frameTime = t1 - t2
@@ -76,36 +99,18 @@ function MainLoop() {
 
     //console.log(frameTime)
 
-    i = 0
-    
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-
-            curVal = 0;
-
-            if (y + x > height  * 2) {
-                curVal = 255
-            }
-            
-            data[i] = curVal; // red
-            data[i + 1] = curVal; // green
-            data[i + 2] = curVal; // blue
-
-            i+= 4
-
-            
-        }
-        
-    }
-
-    ctx.putImageData(imageData, 0, 0);
+    ctx.putImageData(noiseSave, 0, 0);
 
     ballArray.forEach(ball => {
+        ball.vel = Vector.add(ball.vel, ball.accel)
         ball.pos = Vector.add(ball.pos, ball.vel)
         
 
         ctx.beginPath();
         ctx.arc(ball.pos.x, ball.pos.y, ball.radius, 0, 2 * Math.PI);
+        ctx.moveTo(ball.pos.x, ball.pos.y)
+        angleVector = Vector.mul(Vector.normalize(ball.vel), ball.radius)
+        ctx.lineTo(ball.pos.x + angleVector.x, ball.pos.y + angleVector.y)
         ctx.strokeStyle = `hsl(${ball.Hue}, 100%, ${ball.Lightness}%)`
         ctx.stroke();
 
@@ -114,7 +119,7 @@ function MainLoop() {
         for (let pos of pointsInCircle(ball.pos.x, ball.pos.y, ball.radius)){
             
             var pos1d = Dimension2to1(pos.x, pos.y, width)
-            if (data[pos1d * 4] > 0) {
+            if (noiseSaveData[pos1d * 4] > 0) {
                 ball.Hue = "10"
                 
                 ball.vel = new Vector(0, 0)
@@ -136,7 +141,7 @@ function MainLoop() {
             
             pushVector = Vector.normalize(totVector)
             
-            ball.vel = pushVector
+            ball.vel = Vector.mul(pushVector, 10)
 
         }
         
@@ -153,6 +158,9 @@ MainLoop()
 canvasDayList.push(day)
 
 function* pointsInCircle(x, y, r) {
+    r++
+    x = Math.floor(x)
+    y = Math.floor(y)
 
     for (let xpos = x - r; xpos <= x + r; xpos++) {
         for (let ypos = y - r; ypos <= y + r; ypos++) {
@@ -167,9 +175,9 @@ function* pointsInCircle(x, y, r) {
 }
 
 function Dimension2to1(x, y, width) {
-    return x + y * width
+    return  Math.floor(x + y * width)
 }
 
 function Dimension1to2(i, width) {
-    return [x % width, Math.floor(i / width)]
+    return [Math.floor(x % width), Math.floor(Math.floor(i / width))]
 }
